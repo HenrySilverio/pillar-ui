dfe3bb62070d6f16f826cff5924fdbaea137e5d3
HenrySilverio <silveriohenriqueb@gmail.com>
2025-05-26 16:47:43 -0300

    Add Divider component with Storybook integration
    Introduces a Divider component with adjustable width, height, and color options, supporting optional children for custom content.
    
    Integrates the Divider with Storybook, including stories for primary, default, and configured variations, enabling visual testing and documentation.
    
    Adds the `classnames` dependency for dynamic class management.

---
+++ components/Divider/Divider.stories.ts
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Divider, { DividerProps } from './Divider'
+
+const meta: Meta<DividerProps> = {
+  title: 'Molecules/Divider',
+  component: Divider,
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+  }
+}
+
+export default meta
+
+export const Primary: StoryObj<DividerProps> = {
+  args: {}
+}
+
+export const Default: StoryObj<DividerProps> = {
+  args: {}
+}
+
+export const DividerWithProps: StoryObj<DividerProps> = {
+  args: {
+    children: 'Hello World!'
+  }
+}
--- components/Divider/Divider.tsx
+++ components/Divider/Divider.tsx
@@ -1,5 +1,38 @@
-export type DividerProps = {}
+import classNames from "classnames";
 
-const Divider = ({}: DividerProps) => {
-  
+export type DividerProps = {
+  width?: string;
+  height?: string;
+  bgColor?: 'ligth' | 'dark' | 'black';
+  className?: string;
+  children?: React.ReactNode;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+const colorClassMap = {
+  ligth: 'bg-ligth',
+  dark: 'bg-dark',
+  black: 'bg-black',
 }
+
+const Divider = ({
+  width,
+  height='h-[1px]',
+  children,
+  bgColor,
+  ...res
+}: DividerProps) => {
+  const colorClass = colorClassMap[bgColor || 'black'];
+  const barClass = classNames(
+    children ? 'w-1/3' : 'w-1/2',
+    height,
+    colorClass,
+  )
+  return (
+  <div className={classNames(width, 'flex items-center justify-center')}>
+    <div className={barClass}></div>
+    {children && <div className="px-3">{children}</div>}
+    <div className={barClass}></div>
+  </div>
+)
+};
+export default Divider;

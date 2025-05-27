bbed6149571e1b875c8e2884643524156d9a7a0e
HenrySilverio <silveriohenriqueb@gmail.com>
2025-05-27 16:20:12 -0300

    Adds @heroicons/react dependency
    Introduces @heroicons/react version 2.2.0 to the project dependencies for utilizing Heroicons in React components. Updates the lock file to reflect the new package installation.

---
+++ components/Avatar/Avatar.tsx
@@ -0,0 +1,46 @@
+import classNames from 'classnames';
+import AvatarIcon from './AvatarIcon';
+import AvatarImage from './AvatarImage';
+
+export type AvatarProps = {
+  size?: 'xs' | 'sm' | 'md' | 'lg';
+  image?: string;
+  description?: string;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+const avatarSizeMap = {
+  xs: 'w-5 h-5',
+  sm: 'w-6 h-6',
+  md: 'w-8 h-8',
+  lg: 'w-9 h-9',
+};
+
+const Avatar = ({
+  image,
+  description = '',
+  size = 'xs',
+  className,
+  ...res
+}: AvatarProps) => {
+  const avatarSizeMapClass = avatarSizeMap[size] || avatarSizeMap.xs;
+
+  const AvatarContent = image ? (
+    <AvatarImage src={image} altDescription={description} />
+  ) : (
+    <AvatarIcon />
+  );
+
+  return (
+    <div
+      className={classNames(
+        'relative rounded-full bg-slate-100 flex items-center justify-center text-slate-400',
+        avatarSizeMapClass,
+      )}
+      {...res}
+    >
+      {AvatarContent}
+    </div>
+  );
+};
+
+export default Avatar;

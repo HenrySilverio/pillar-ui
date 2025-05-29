d94c3a2bea3e5ac11290320ad19c2097083b93a3
HenrySilverio <silveriohenriqueb@gmail.com>
2025-05-29 11:34:15 -0300

    Adds new typography stories and refines variants
    Introduces additional stories for typography components, including headings and paragraphs, to enhance documentation and testing.
    
    Refines text variants to improve consistency and readability, with adjustments to default values and formatting.

--- components/Typography/Typography.stories.ts
+++ components/Typography/Typography.stories.ts
@@ -15,6 +15,42 @@ export default meta
 
 export const Primary: StoryObj<TypographyProps> = {
   args: {
-    children: 'Text'
+    children: 'Texto'
+  }
+}
+
+export const TextHeadingH1: StoryObj<TypographyProps> = {
+  args: {
+    children: "Texto",
+    element: "h1",
+    size: "title1",
+    className: "font-extrabold",
+  },
+};
+
+export const TextHeadingH2: StoryObj<TypographyProps> = {
+  args: {
+    children: "Texto",
+    element: "h2",
+    size: "title2",
+    className: "font-extrabold",
+  },
+};
+
+export const TextHeadingH3: StoryObj<TypographyProps> = {
+  args: {
+    children: "Texto",
+    element: "h3",
+    size: "title3",
+    className: "font-extrabold",
+  },
+};
+
+export const TextParagraph: StoryObj<TypographyProps> = {
+  args: {
+    children: 'Text',
+    element: 'p',
+    size: 'md',
+    className: 'text-primary'
   }
 }

426c9a95f6077c5fc0a69e5690e69d6705dba9eb
HenrySilverio <silveriohenriqueb@gmail.com>
2025-05-29 12:09:21 -0300

    Refactor code structure for improved readability and maintainability
    Add Storybook manager API and theming dependencies
    
    Includes new dependencies for Storybook manager API and theming
    to enhance modularity and maintainability. Updates `yarn.lock`
    to reflect the changes.

---
+++ .storybook/pillarUi.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Pillar UI',
+  brandUrl: 'https://github.com/HenrySilverio/pillar-ui',
+  brandImage:'/pillarui.png',
+  brandTarget: '_self',
+})
